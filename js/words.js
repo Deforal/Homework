541ffb5d6941ea1959bea126dbf86bd91e55b180
Deforal <epikego@mail.ru>
2023-05-12 17:33:05 +0800

    new changes(words G)

--- js/words.js
+++ js/words.js
@@ -1,10 +1,9 @@
 function words() {
     const arr = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
-    let newarr = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
-    newarr = newarr.sort(() => Math.random() - 0.5);
-    alert(newarr)
-    let first = prompt('Какой был первый элемент массива? (элементы были перемешаны)')
-    let last = prompt('Какой был последний элемент массива? (элементы были перемешаны)')
+    arr = arr.sort(() => Math.random() - 0.5);
+    alert(arr)
+    let first = prompt('Какой был первый элемент массива?')
+    let last = prompt('Какой был последний элемент массива?')
     if ((first.toLowerCase() === arr[0].toLowerCase()) && (last.toLowerCase() === arr[arr.length - 1].toLowerCase())){
         alert('Вы угадали!')
     } else if ((first.toLowerCase() === arr[0].toLowerCase()) || (last.toLowerCase() === arr[arr.length - 1].toLowerCase())) {
